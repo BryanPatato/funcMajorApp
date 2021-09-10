a046b3bd7d2306b853a0dcb8faf0ee71e2901e25
bryan <bryan.ruiz23@d155.org>
2021-09-10 13:42:56 -0500

    all

---
+++ funcMajorApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  funcMajorApp
+//
+//  Created by BRYAN RUIZ on 9/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
